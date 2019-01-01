4dd9eb77ae659248a26b444399e99b57231a45ce
schatzju <schatzju@umich.edu>
2019-01-01 17:21:12 -0500

    first

---
+++ js/navigate.js
@@ -0,0 +1,12 @@
+document.getElementById('art_button').onclick = function(event) {
+   location.href = 'art.html';
+}
+
+document.getElementById('research_button').onclick = function(event) {
+   location.href = 'research.html';
+}
+
+document.getElementById('blog_button').onclick = function(event) {
+   location.href = 'https://paper.dropbox.com/doc/Blog--AUrycgOz0Kb2xr~Y~ZYUiLRlAQ-lMaBjTlFcRizi4zllV6Qd';
+}
+
